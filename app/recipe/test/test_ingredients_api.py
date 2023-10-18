6d25b4e8a44270bcf82d5641ff11e127c0efc39b
Tamara MT <tamaramolinast@gmail.com>
2023-10-18 07:54:11 -0300

    test for updating ingredients

--- app/recipe/test/test_ingredients_api.py
+++ app/recipe/test/test_ingredients_api.py
@@ -16,6 +16,11 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
+def detail_url(ingredient_id):
+    """Create and return an ingredient detail URL"""
+    return reverse('recipe:ingredient-detail', args=[ingredient_id])
+
+
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return user"""
     return get_user_model().objects.create_user(email=email, password=password)
@@ -66,3 +71,15 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], ingredient.name)
         self.assertEqual(response.data[0]['id'], ingredient.id)
+
+    def test_update_ingredient(self):
+        """Test updating an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Cilantro')
+
+        payload = {'name': 'Coriander'}
+        url = detail_url(ingredient.id)
+        response = self.client.patch(url, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+        self.assertEqual(ingredient.name, payload['name'])
